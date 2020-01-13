900796c9204aaa1e34545a9a48006b9663cf75a7
AldoEstebanPaz <estebanpazutn@gmail.com>
2020-01-13 20:44:57 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
+import Axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      products: []
+    };
+  }
+
+  async componentDidMount() {
+    const res = await Axios.get('/api/products');
+    console.log(res.data);
+    this.setState({ products: res.data });
+  }
+
+  render() {
+
+    // const products = this.state.products;
+
+    // const htmlproducts = [];
+    // if (products !== null) {
+    //   for(let i = 0; i< products.length ; i++) {
+    //     htmlproducts.push(
+    //       <div>{products[i].brand} - {products[i].name}</div>
+    //     );
+    //   }
+    // }
+
+    // let htmlproductsWithMap = [];
+    // if (products !== null) {
+    //   htmlproductsWithMap = products.map(
+    //     p => <div>{p.brand} - {p.name}</div>
+    //   ); 
+    // }
+
+    const clients = [
+      { name: "John", lastName: "Doe" },
+      { name: "Rubber", lastName: "Duck" },
+      { name: "Anonymous", lastName: "-" },
+    ];
+    const htmlClients = [];
+
+    for(let i = 0; i < clients.length; i++) {
+      htmlClients.push(
+        <li>{clients[i].name}, {clients[i].lastName}</li>
+      );
+    }
+
+    return (
+      <ul>
+        {htmlClients}
+      </ul>
+    );
+
+
+  }
 }
 
 export default App;
---
+++ src/prueba.js
@@ -0,0 +1,14 @@
+
+
+let numbers = [ 1, 2, 3 ];
+
+numbers = numbers.map(
+  n => n * 2
+);
+
+// result:
+//   numbers = [ 2, 4, 6 ]
+
+
+
+console.log(numbers);
